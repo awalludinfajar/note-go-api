2cc641a524a7eb8ee1388ffc9064fadec0a00586
Awalludin Fajar <fajarawalludin25@gmail.com>
2024-12-10 16:59:45 +0700

    test-aplikasi-note-bts.id

--- app/model/noteModel.go
+++ app/model/noteModel.go
@@ -2,7 +2,7 @@ package model
 
 type Note struct {
 	ID      int    `json:"id"`
-	UserId  int    `json:"userId"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
+	// UserId  int    `json:"userId"`
 }
